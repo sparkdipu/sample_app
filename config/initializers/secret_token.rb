8cc9d80abbfc5ebd8989c846171f6be7177c2819
Dipu <dipu@sparksupport.com>
2011-08-02 14:45:16 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1aa245c62f5bd288f53c16a4b315bc3a4996c7aa56767ad8d03b589041403767291fc9eee44921992d504ab86e9951b04ca4f9f2daf95e6d228838a5c692dae2'
