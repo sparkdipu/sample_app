6880df5b53108c9d77c5c16bdeae1371a6c7838b
Dipu <dipu@sparksupport.com>
2011-08-04 17:05:25 +0600

    New helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  #Return logo image link to the view file
+  def logo
+    image_tag('logo.png', :alt=>"Sample App", :class=>"round")
+  end
+  
   # Return a title on a per-page basis.
   def title 
     baseTitle = "Ruby on Rails Tutorial Sample APP"
