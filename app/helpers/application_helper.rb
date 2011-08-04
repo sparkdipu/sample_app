e91049274a3682d09865489ebd6b8e67cdd18d30
Dipu <dipu@sparksupport.com>
2011-08-04 11:55:55 +0600

    added some dynamic contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title 
+    baseTitle = "Ruby on Rails Tutorial Sample APP"
+    if(@title.nil?)
+      baseTitle
+    else
+      "#{baseTitle} | #{@title}"
+    end
+  end
+  
 end
